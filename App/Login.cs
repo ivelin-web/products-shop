ca4de86c2eabf5b50cbb7fd44af8ca916ebf37f3
Ivelin Ivanov <ivelinivanov1999@gmail.com>
2022-01-29 18:37:09 +0200

    Created Login form.

---
+++ App/Login.cs
@@ -0,0 +1,25 @@
+namespace App
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
